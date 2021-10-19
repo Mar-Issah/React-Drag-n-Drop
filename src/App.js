a09a13280125c7c2a410c05080f1b92226ba9eae
Mar-Issah <masy370@gmail.com>
2021-10-18 22:39:21 -0400

    hello world

--- src/App.js
+++ src/App.js
@@ -1,90 +1,11 @@
-//import logo from './logo.svg';
-import './App.css';
-import CustomHook from './componentHook/CustomHook';
-//import DataFetchingTwo from './componentHook/DataFetchingTwo';
-//import UseMemo from './componentHook/UseMemo';
-//import UseRef from './componentHook/UseRef';
-//import UseRefTimer from './componentHook/UseRefTimer';
-//import IntervalClassCounter from './componentHook/IntervalClassCounter';
-//import ClassMouse from './componentHook/ClassMouse';
-//import MouseContainer from './componentHook/MouseContainer';
-//import HookCounter from './componentHook/HookCounter';
-//import UseEffect from './componentHook/UseEffect';
-//import Greet from './components/siya'
-//import Welcome from './components/siya'
-//import Hello from './components/siya'
-//import Counter from './components/Counter'
-//import EventFile from './components/EventFile'
-//import ClassClick from './components/ClassClick'
-// import EventBind from './components/EventBind'
-// import ParentComponent from './components/ParentComponent'
-//import Conditional from './components/Conditional'
-//import NameList from './components/NameList'
-//import Stylesheet from './components/Stylesheet'
-// import Form from './components/Form'
-// import  Lifecycle from './components/Lifecycle'
-//import Fragment from './components/Fragment'
-//import  Columns from './components/Columns'
-//import ParentCompo from './components/ParentCompo';
-//import Refs from './components/Refs';
-//import ErrorBoundary  from './components/ErrorBoundary.js';
-//import Portal  from './components/Portal.js';
-// import HoverCounter  from './components/HoverCounter.js';
-// import ClickCounter  from './components/ClickCounter.js';
-//import ComponentC from './components/ComponentC';
-//import PostList from './components/PostList';
-//import { UserProvider } from './components/userContext';
-//import PostForm from './components/PostForm';
-//import DataFetching from './componentHook/DataFetching';
-
-
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <CustomHook/>
-    {/* <DataFetchingTwo/> */}
-      {/* <ReducerCounterThree/> */}
-       {/* <DataFetching/> */}
-      {/* <IntervalClassCounter /> */}
-      {/* <MouseContainer/> */}
-      {/* <ClassMouse/> */}
-     {/* <UseEffect/> */}
-      {/* <HookCounter/> */}
-    {/* <PostForm/> */}
-     {/* <PostList/> */}
-      {/* <ClickCounter />
-      <HoverCounter name="Siya"/> */}
-      {/* <ErrorBoundary>
-        <Hero heroName = 'Batman' />
-        <Hero heroName='superman' />
-        <Hero heroName = 'Joker' />
-      </ErrorBoundary> */}
-{/* 
-      <Portal/> */}
-       {/* <ForwParentInput/> */}
-      {/* <Refs /> */}
-      {/* <ParentCompo/> */}
-      {/* <Fragment />
-      <Columns/> */}
-       {/* <Lifecycle/> */}
-      {/* <Form/> */}
-      {/* <Stylesheet primary={false}/> */}
-       {/* <NameList/> */}
-      {/* <Conditional/> */}
-      {/* <ParentComponent/> */}
-      {/* <EventBind /> */}
-      {/* <EventFile />
-      <ClassClick /> */}
-      {/* <Counter /> */}
-      {/* <Greet name="Siya" heroName="Superman">
-        <button>Action</button>
-      </Greet>
-      <Greet name="Diana"  heroName ="Wonder Woman" /> */}
-        {/* <Welcome /> 
-        <Hello /> */}
-    </div>
-  );
+	return (
+		<div>
+			<h1>hey</h1>
+		</div>
+	);
 }
 
 export default App;
